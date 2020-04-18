f106f6e59732a98c3ac32510d5b40003036a41fd
gudodo16 <31950075+gudodo16@users.noreply.github.com>
2020-04-18 17:43:43 +0700

    18/4/2020
    c2

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from "../hero";
+import { HEROES } from "../hero";
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  selectedHero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
